a07c42a23ba7a7906a311c0a85312ac307d42551
Richard Hadden <richard.hadden@oeaw.ac.at>
2026-04-13 14:43:09 +0200

    initialise createdb models

--- pangloss/model_setup/initialise_action_classes/initialise_create_db_model.py
+++ pangloss/model_setup/initialise_action_classes/initialise_create_db_model.py
@@ -378,7 +378,7 @@ def get_relation_annotation_types(
         return field_definition.wrapper[  # type: ignore
             Annotated[Union[*types], Field(discriminator="type")]
         ]
-    return Union[*types]  # ty:ignore[invalid-type-form]
+    return Union[*types]  # type: ignore # ty:ignore[invalid-type-form]
 
 
 def get_embedded_annotation_types(
